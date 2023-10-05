9500ffc8fe5eb51c3c770ed3c4db18f33a83a134
JoseMaria16 <josejaureguirivas@gmail.com>
2023-10-05 18:02:19 -0500

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "home": "linear-gradient(149deg, #ffffff 45%, #5e93eb 98%, #ffffff 100%);",
+        "text" : "linear-gradient(90deg, rgba(202,60,60,1) 0%, rgba(235,53,53,1) 25%, rgba(51,73,84,1) 72%, rgba(0,0,0,1) 100%)",
+        "button": "linear-gradient(90deg, rgba(202,60,60,1) 0%, rgba(235,53,53,1) 25%, rgba(51,73,84,1) 72%, rgba(0,0,0,1) 100%)"
       },
     },
   },
